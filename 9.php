acfae94daa96f028eb4035fce855bf9fc6980083
AbiyuRafi <abiyurafii09@gmail.com>
2023-08-22 10:09:29 +0700

    algoritma

--- 9.php
+++ 9.php
@@ -1,76 +1,67 @@
-<?php
-
-$no_pegawai;
-$no_golongan;
-$tanggal;
-$bulan;
-$tahun;
-$no_urut;
-$tanggal_lahir;
-
-if (isset($_POST['submit'])) {
-    $no_pegawai = $_POST['nomer'];
-
-    if (strlen($no_pegawai) < 11) {
-        echo "No pegawai tidak sesuai";
-    } else {
-        $no_golongan = strtr($no_pegawai, 0, 1);
-        $tanggal = strtr($no_pegawai, 1, 2);
-        $bulan = strtr($no_pegawai, 3, 2);
-        $tahun = strtr($no_pegawai, 5, 4);
-        $no_urut = strtr($no_pegawai, 9, 2);
-
-        if ($bulan == "01") {
-            echo "Bulan: Januari";
-        } elseif ($bulan == "02") {
-            echo "Bulan: Februari";
-        } elseif ($bulan == "03") {
-            echo "Bulan: Maret";
-        } elseif ($bulan == "04") {
-            echo "Bulan: April";
-        } elseif ($bulan == "05") {
-            echo "Bulan: Mei";
-        } elseif ($bulan == "06") {
-            echo "Bulan: Juni";
-        } elseif ($bulan == "07") {
-            echo "Bulan: Juli";
-        } elseif ($bulan == "08") {
-            echo "Bulan: Agustus";
-        } elseif ($bulan == "09") {
-            echo "Bulan: September";
-        } elseif ($bulan == "10") {
-            echo "Bulan: Oktober";
-        } elseif ($bulan == "11") {
-            echo "Bulan: November";
-        } else {
-            echo "Bulan: Desember";
-        }
-    }
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Kode Pegawai</title>
 </head>
-
 <body>
-    <form action="" method="post">
-        <table>
-            <tr>
-                <td>No Pegawai</td>
-                <td><input type="text" name="nomer" value=></td>
-            </tr>
-            <tr>
-                <td><input type="submit" name="submit" value="Submit"></td>
-            </tr>
-        </table>
+    <form method="POST" action="">
+        <tr>
+            <td>Input Kode Pegawai</td>
+            <td><input type="number" value="kode pegawai" name="bilangan"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Submit" name="submit"></td>
+        </tr>
     </form>
-</body>
+    <?php
+    if (isset($_POST['submit'])) {
+        $kodepegawai = $_POST['bilangan'];
+        $golongan = substr($kodepegawai, 0, 1);
+        $tanggal = substr($kodepegawai, 1, 2);
+        $bulan = substr($kodepegawai, 3, 2);
+        $tahun = substr($kodepegawai, 5, 4);
+        $urutan = substr($kodepegawai, 12, 1);
 
+        if ($kodepegawai < 11) {
+            echo "nomor pegawai tidak sesuai";
+        } else {
+            $nama_bulan = "";
+
+            if ($bulan == "01") {
+                $nama_bulan = "Januari";
+            } elseif ($bulan == "02") {
+                $nama_bulan = "Februari";
+            } elseif ($bulan == "03") {
+                $nama_bulan = "Maret";
+            } elseif ($bulan == "04") {
+                $nama_bulan = "April";
+            } elseif ($bulan == "05") {
+                $nama_bulan = "Mei";
+            } elseif ($bulan == "06") {
+                $nama_bulan = "Juni";
+            } elseif ($bulan == "07") {
+                $nama_bulan = "Juli";
+            } elseif ($bulan == "08") {
+                $nama_bulan = "Agustus";
+            } elseif ($bulan == "09") {
+                $nama_bulan = "September";
+            } elseif ($bulan == "10") {
+                $nama_bulan = "Oktober";
+            } elseif ($bulan == "11") {
+                $nama_bulan = "November";
+            } elseif ($bulan == "12") {
+                $nama_bulan = "Desember";
+            }
+
+            $tanggal_lahir = "$tanggal $nama_bulan $tahun";
+
+            echo "Nomor Golongan: $golongan<br>";
+            echo "Tanggal Lahir: $tanggal_lahir <br>";
+            echo "Nomor Urut Pegawai: $urutan<br>";
+        }
+    }
+    ?>
+</body>
 </html>
