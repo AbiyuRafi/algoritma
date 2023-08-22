a301da6c6b31ac9422d0e4ffe768f7b9ecb5e125
AbiyuRafi <abiyurafii09@gmail.com>
2023-08-22 23:23:40 +0700

    algoritma

--- 18.php
+++ 18.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <y lang="en">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
     </head>
+
     <body>
-        <form action="" method="post" >
+        <form action="" method="post">
             <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $siswa = array();
@@ -26,7 +28,7 @@
                 echo "<input type='submit' value='Mulai'>";
             }
             ?>
-            
+
             <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nilai']) && isset($_POST['kehadiran'])) {
                 $siswa = $_POST['nilai'];
