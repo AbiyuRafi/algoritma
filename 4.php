cb9b98a126aa9f911163026aff30bc0eb1ae6fae
AbiyuRafi <abiyurafii09@gmail.com>
2023-08-23 08:45:49 +0700

    algoritma

--- 4.php
+++ 4.php
@@ -1,44 +1,49 @@
-<?php
-
-$tunj = 0;
-$pjk = 0;
-$gaji_bersih = 0;
-$gaji_pokok = 0;
-$name = "";
-
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $gaji_pokok = $_POST['gaji_pokok'];
-    $gaji_bersih = $gaji_pokok - ($gaji_pokok * 0.1);
-    $pjk = $gaji_bersih - ($gaji_bersih * 0.1);
-    $tunj = $pjk - ($pjk * 0.1);
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>algo4</title>
 </head>
 
 <body>
-    <form action="" method="post">
-        <input type="text" name="name" required placeholder="Nama" value="<?php echo $name; ?>"><br>
-        <input type="number" name="gaji_pokok" required placeholder="Gaji" value="<?php echo $gaji_pokok; ?>"><br>
-        <input type="submit" value="submit" name="submit">
-    </form>
+    <table>
+        <form action="" method="post">
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td>Gaji Pokok</td>
+                <td>:</td>
+                <td><input type="number" name="gaji"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><input type="submit" name="submit" value="Hitung"></td>
+            </tr>
+        </form>
+    </table>
 
-    <?php if (isset($_POST['submit'])) : ?>
-        <p>Nama: <?php echo $name; ?></p>
-        <p>Gaji Pokok: <?php echo $gaji_pokok; ?></p>
-        <p>Gaji Bersih: <?php echo $gaji_bersih; ?></p>
-        <p>Pajak: <?php echo $pjk; ?></p>
-        <p>Tunjangan: <?php echo $tunj; ?></p>
-    <?php endif; ?>
+    <?php
+
+    if (isset($_POST['submit'])) {
+        $nama = $_POST["name"];
+        $gajiPokok = $_POST['gaji'];
+
+        $tunjangan = (20 * $gajiPokok) / 100;
+        $pjk = (15 * ($gajiPokok + $tunjangan)) / 100;
+        $gajibersih =  $gajiPokok + $tunjangan - $pjk;
+
+        echo "Nama : " . $nama . "<br>";
+        echo "Tunjangan Jabatan: Rp. " . number_format($tunjangan) . "<br>";
+        echo "Pajak PJK: Rp. " . number_format($pjk) . "<br>";
+        echo "Total Gaji Bersih: Rp. " . number_format($gajibersih) . "<br>";
+    }
+    ?>
 </body>
 
 </html>
